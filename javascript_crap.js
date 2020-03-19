8148feb3d00aae97608054ca2886f9e2e8f3af32
marksherif <marksherifjoseph@gmail.com>
2020-03-19 02:29:21 +0200

    slow down rain animation

--- javascript_crap.js
+++ javascript_crap.js
@@ -10,20 +10,22 @@ function makeItRain() {
         //couple random numbers to use for various randomizations
         //random number between 98 and 1
         var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
-        //random number between 5 and 2
-        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+        //random number between 17 and 2
+        var randoFiver = (Math.floor(Math.random() * (17 - 2 + 1) + 2));
         //increment
         increment += randoFiver;
         //add in a new raindrop with various randomizations to certain CSS properties
-        drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.8' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.8' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.8' + randoHundo + 's;"></div></div>';
-        backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.8' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.8' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.8' + randoHundo + 's;"></div></div>';
+        drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 1.0' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 1.0' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 1.0' + randoHundo + 's;"></div></div>';
+        // backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 1.0' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 1.0' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 1.0' + randoHundo + 's;"></div></div>';
     }
 
     $('.rain.front-row').append(drops);
-    $('.rain.back-row').append(backDrops);
+    // $('.rain.back-row').append(backDrops);
 }
 
 window.onload = function() {
+    $('body').toggleClass('splat-toggle');
+    $('.splat-toggle.toggle').toggleClass('active');
     this.makeItRain()
 };
 
